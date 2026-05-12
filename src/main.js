29b1c8f9285564729c1681458f79ea1ee7e633c0
benoitmondjo <benoitmondjo@gmail.com>
2026-05-12 03:25:05 +0100

    projet termine

--- src/main.js
+++ src/main.js
@@ -1,60 +1,134 @@
-import './style.css'
-import javascriptLogo from './assets/javascript.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import { setupCounter } from './counter.js'
+import './main.scss';
 
-document.querySelector('#app').innerHTML = `
-<section id="center">
-  <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${javascriptLogo}" class="framework" alt="JavaScript logo"/>
-    <img src="${viteLogo}" class="vite" alt="Vite logo" />
-  </div>
-  <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/main.js</code> and save to test <code>HMR</code></p>
-  </div>
-  <button id="counter" type="button" class="counter"></button>
-</section>
+function createAndStyleElement(tag, className = '', content = ''){
+  
+  const element = document.createElement(tag)
+  if(className) element.className = className;
+  if(content) element.innerHTML = content;
 
-<div class="ticks"></div>
+  return element;
 
-<section id="next-steps">
-  <div id="docs">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#documentation-icon"></use></svg>
-    <h2>Documentation</h2>
-    <p>Your questions, answered</p>
-    <ul>
-      <li>
-        <a href="https://vite.dev/" target="_blank">
-          <img class="logo" src="${viteLogo}" alt="" />
-          Explore Vite
-        </a>
-      </li>
-      <li>
-        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-          <img class="button-icon" src="${javascriptLogo}" alt="">
-          Learn more
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div id="social">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#social-icon"></use></svg>
-    <h2>Connect with us</h2>
-    <p>Join the Vite community</p>
-    <ul>
-      <li><a href="https://github.com/vitejs/vite" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
-      <li><a href="https://chat.vite.dev/" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#discord-icon"></use></svg>Discord</a></li>
-      <li><a href="https://x.com/vite_js" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#x-icon"></use></svg>X.com</a></li>
-      <li><a href="https://bsky.app/profile/vite.dev" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#bluesky-icon"></use></svg>Bluesky</a></li>
-    </ul>
-  </div>
-</section>
+}
 
-<div class="ticks"></div>
-<section id="spacer"></section>
-`
+function showSection(section){
 
-setupCounter(document.querySelector('#counter'))
+  document.querySelectorAll('.section').forEach(sec => sec.classList.remove('active'))
+  section.classList.add('active')
+}
+
+async function fetchData(){
+  const dataContainer = document.querySelector('.data-container');
+  dataContainer.innerHTML = "";
+
+  const loadingElement = createAndStyleElement('div', 'loading', 'chargement en cours...');
+  dataContainer.append(loadingElement)
+
+  try{
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+    const data = await res.json();
+
+     setTimeout(()=>{
+      dataContainer.removeChild(loadingElement);
+      data.slice(0, 5).forEach(item => {
+        const title = createAndStyleElement("h2", '', item.title);
+        const body = createAndStyleElement("p", '', item.body)
+      
+        dataContainer.appendChild(title)
+        dataContainer.appendChild(body)
+      })
+     }, 600)
+  }
+  catch(err){
+    dataContainer.removeChild(loadingElement);
+    dataContainer.textContent = " Une erreur s'est produite. Veuillez reesseyez plutard."
+  }
+}
+
+function setupCounter(element){
+  let counter = 0;
+  const setCounter = (count)=>{
+    counter = count;
+    element.innerHTML = `<button>${count}</button>`
+  }
+  element.addEventListener('click', ()=> setCounter(counter+1));
+  setCounter(0)
+}
+
+function closeSubMenu(){
+  const submenu = document.querySelector('.sub-menu');
+  if(submenu.classList.contains('active')){
+    submenu.classList.remove('active')
+  }
+}
+
+function createPage(){
+  const app =  document.getElementById("app");
+
+  const nav = createAndStyleElement("nav");
+  const homeLink = createAndStyleElement("a", '', 'Home');
+  const aboutLink = createAndStyleElement("a", '','A propos');
+  const fetchDataLink = createAndStyleElement("a", '', 'Fetch');
+  const networkLink = createAndStyleElement("a", '', 'Reseaux');
+
+  const mainContent = createAndStyleElement("div",  'main-content');
+  const footer = createAndStyleElement("footer", '', `<p>&copy 2026 JAVASCRIPT DOM, Tous droits reserves. </p> <p> <a href="#" target="_blank">Twitter X</a>|
+	<a href="#" target="_blank">Facebbok</a>|
+	<a href="#" target="_blank">Linkedine</a> </p>`);
+
+  const homeSection = createAndStyleElement("div", 'section active',
+    `<h2>Bienvebu sur JS DOM</h2> <p>Cliquez le button pour augmenter le compteur</p> <div id="counter" class="counter"></div>`
+  )
+
+  const aboutSection = createAndStyleElement('div', 'section', `Page entierement cree en JS.`)
+
+  const dataSection = createAndStyleElement('div', 'section data-container')
+
+  nav.append(homeLink)
+  nav.append(aboutLink)
+  nav.append(fetchDataLink)
+  nav.append(networkLink)
+  app.append(nav)
+
+  const submenu = createAndStyleElement('div', 'sub-menu', `<a href="#" target="_blank">Twitter X</a>  <a href="#" target="_blank">Facebbok</a> <a href="#" target="_blank">Linkedine</a>`)
+  nav.append(submenu);
+  document.addEventListener('click', (e)=>{
+    if(!nav.contains(e.target)){
+      submenu.classList.remove('active')
+    }
+  })
+
+  app.append(mainContent)
+  app.append(footer)
+
+  mainContent.append(homeSection)
+  mainContent.append(aboutSection)
+  mainContent.append(dataSection)
+
+
+  homeLink.addEventListener('click', ()=>{
+      showSection(homeSection)
+      closeSubMenu();
+  })
+
+  aboutLink.addEventListener('click', ()=>{
+      showSection(aboutSection)
+      closeSubMenu();
+  })
+
+  fetchDataLink.addEventListener('click', ()=>{
+      showSection(dataSection);
+      fetchData();
+      closeSubMenu();
+  })
+
+  networkLink.addEventListener('click', (e)=>{
+      e.stopPropagation();
+      submenu.classList.toggle('active')
+  })
+
+  const counterElement = document.getElementById("counter")
+  setupCounter(counterElement)
+
+}
+
+createPage();
